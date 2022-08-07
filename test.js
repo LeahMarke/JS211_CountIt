58d335d7640767fb148dc120037091dbb31cc557
leahmarke <lmarke@live.com>
2022-08-07 15:13:51 -0400

    finished code without testing

--- test.js
+++ test.js
@@ -7,32 +7,55 @@
 //   output: process.stdout,
 // });
 
+const userInput = "This is my7 sent&ence fr?om the user";
+console.log(userInput);
+const noSpecialCharacters = userInput.replace(/[^a-zA-Z]/g, "").toLowerCase();
+console.log(typeof noSpecialCharacters);
+console.log(noSpecialCharacters);
 
-const userInput = "This is my sentence from the user";
-const userStrToArr = userInput.split("");
-console.log(userStrToArr);
+const newArray = Array.from(noSpecialCharacters);
+console.log(newArray);
+// const noSpecialArray = noSpecialCharacters.split("");
+// console.log(noSpecialArray);
 
-const finalLetterCounter = userStrToArr.map((letters) => {
-  const charCount = (string, letter) => {
-    let letterCount = 0;
-    for(i=0, i<string.length; i++) {
-  if(string.charAt(i) == letter) {
-    letterCount += 1;
-  }
-    }
-    return letterCount;
-  };
-  
+let counter = (arr) => {
+  return arr.reduce((acc, cur) => {
+    acc[cur] ? acc[cur]++ : (acc[cur] = 1);
+    return acc;
+  }, {});
+};
+counter(newArray);
+console.log(counter(newArray));
+const counterObject = counter(newArray);
+console.log(counterObject);
+const keys = Object.keys(counterObject);
+console.log(keys);
+keys.forEach((key) => {
+  const result = `There are ${counterObject[key]} - ${key.toUpperCase()}'s`;
+  console.log(result);
 });
+
+// const finalLetterCounter = userStrToArr.map((letters) => {
+//   const charCount = (string, letter) => {
+//     let letterCount = 0;
+//     for(i=0, i<string.length; i++) {
+//   if(string.charAt(i) == letter) {
+//     letterCount += 1;
+//   }
+//     }
+//     return letterCount;
+//   };
+
+// });
 // const justLetters = userStrToArr.filter(lettersOnly);
 
-const lettersOnly = userStrToArr.filter((letter, index) => {
-  console.log(userStrToArr.indexOf(letter), index);
-  if (userStrToArr.indexOf(letter) == index) {
-    console.log(`${letter} appears `);
-  }
-});
-console.log(lettersOnly);
+// const lettersOnly = userStrToArr.filter((letter, index) => {
+//   console.log(userStrToArr.indexOf(letter), index);
+//   if (userStrToArr.indexOf(letter) == index) {
+//     console.log(`${letter} appears `);
+//   }
+// });
+// console.log(lettersOnly);
 // const getPrompt = () => {
 //   rl.question("what is your sentence?: ", (string) => {
 //     charCount(string, letter);
