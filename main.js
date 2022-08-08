7b3eff32f0dbb9eea0c76cb2112a5e49495c539e
leahmarke <lmarke@live.com>
2022-08-08 01:09:02 -0400

    added x 1 test, failing

--- main.js
+++ main.js
@@ -26,17 +26,3 @@ const displayResults = () => {
     );
   });
 };
-// const sentencePigLatin = (sentence) => {
-//   // place all words into an Array , then capture array
-//   sentence = document.getElementById("user-input").value;
-//   let newArray = sentence.split(" ");
-//   // [I, am, happy, today]
-//   // need to seperate each word out into it's own array
-//   let seperateWord = newArray.map((word) => pigLatin(word));
-//   // loop through each word in the array and then each character like previously
-//   // join back to a string
-//   let newPigSentence = seperateWord.join(" ");
-//   const newElement = document.createElement("p");
-//   newElement.innerHTML = newPigSentence;
-//   document.getElementById("display-element").appendChild(newElement);
-// };
